ecd2b17bc6334c9606a6c775b32aff94a3a0ff82
philzphaz <bour.phileas@gmail.com>
2026-02-02 21:59:20 +0100

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="frenchplotlib",
-    version="0.4.0",
+    version="0.5.0",
     description="French-themed matplotlib markers and colormaps",
     author="philzphaz",
     url="https://github.com/PhilZPhaZ/frenchplotlib",
